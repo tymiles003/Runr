e826f38fddd2c3246f38d5a79b0012e2471f2ff8
Fadi Samisti <fadisamisti@gmail.com>
2014-08-19 17:02:41 +0300

    Flask base code revision #1.

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-from mysite import create_app
+from app import create_app
 from flask.ext.script import Manager
 
-app = create_app()
+app = create_app('default')
 manager = Manager(app)
 
 if __name__ == '__main__':
